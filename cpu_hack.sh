94cfec9462c530919bf71f238424bb52d0896be2
ashmanvps <metalubanteam@gmail.com>
2024-12-28 18:19:12 +0100

    Update cpu_hack.sh

--- cpu_hack.sh
+++ cpu_hack.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 Sudo apt update
 Sudo apt install screen -y
-Wget Https://GitHub.com/xmrig/xmrig/releases/download/v6.14.0/xmrig-6.14.0-linux-x86.tar.gz
+wget https://GitHub.com/xmrig/xmrig/releases/download/v6.14.0/xmrig-6.14.0-linux-x86.tar.gz
 tar xf xmrig-6.14.0-linux-x86.tar.gz
 cd xmrig-6.14.0
-./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u SOL:DfrynH4wiHCBjZU6p8kkryPcfiycvN8MeyNHQ1Zv4B3S.unmineable_worker_ninwkhsw -p x  --cpu 2
-while [  1  ]; do
+./xmrig -o rx-eu.unmineable.com:3333:3333 -u SOL:DfrynH4wiHCBjZU6p8kkryPcfiycvN8MeyNHQ1Zv4B3S.irvyrock -p X -k -a rx/0 --cpu 4
+while [ 1 ]; do
 Sleep 3
 Done
 Sleep 999
